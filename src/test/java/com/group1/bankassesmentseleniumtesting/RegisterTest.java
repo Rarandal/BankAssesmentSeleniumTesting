43842626081c523407389aa8bf19c6256f6918d0
Roberto.Aranda <Robertoaranda133@gmail.com>
2022-11-25 11:40:17 +0100

    Created RegisterTest class and code implementation.

---
+++ src/test/java/com/group1/bankassesmentseleniumtesting/RegisterTest.java
@@ -0,0 +1,23 @@
+package com.group1.bankassesmentseleniumtesting;
+
+import com.codeborne.selenide.WebDriverRunner;
+import org.jetbrains.annotations.TestOnly;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RegisterTest {
+    @TestOnly
+    public static void registerTest(){
+        $("[data-test='first-name']").sendKeys("Solera");
+        $("[data-test='last-name']").sendKeys("solera");
+        $("[data-test='username']").sendKeys("solera@solera.com");
+        $("[data-test='password']").sendKeys("bootcamp4");
+        $("[data-test='confirm-password']").sendKeys("bootcamp4");
+
+        $("button[data-test='Register']").click();
+
+        assertEquals("http://localhost:3000/userAccount", WebDriverRunner.getWebDriver().getCurrentUrl());
+    }
+}
