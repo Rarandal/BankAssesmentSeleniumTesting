b5493915629afe102b1a4ccec78ec73c3c1d4f04
Vicente <Vicente.Higuera@solera.com>
2022-11-25 15:21:51 +0100

    creación checktransactiontest

---
+++ src/test/java/com/group1/bankassesmentseleniumtesting/CheckTransactionTest.java
@@ -0,0 +1,26 @@
+package com.group1.bankassesmentseleniumtesting;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.WebDriverRunner;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;import java.awt.Button;
+
+import static com.codeborne.selenide.Condition.attribute;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.*;
+
+public class CheckTransactionTest {
+
+	public static void checkTransactionTest() {
+		
+		open("/dataEntry");
+		$("button[data-test='data-entry']");
+		
+		
+	}
+
+}
