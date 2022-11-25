67ba1394d55da5bc25a0e0d908bb7359e7798b2f
INT\DanielXabier.Lopez <danielxabier.lopez@solera.com>
2022-11-25 14:59:23 +0100

    se añaden test en las clases BankAccountTest

---
+++ src/test/java/com/group1/bankassesmentseleniumtesting/BankAccountTest.java
@@ -0,0 +1,16 @@
+package com.group1.bankassesmentseleniumtesting;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class BankAccountTest {
+	
+	public void bankAccountTest() {
+		open("/BankAccount");
+		$("button[data-test='Create-Account']");
+		$("button[data-test='Delete-Account']");
+		
+		
+	}
+
+}
