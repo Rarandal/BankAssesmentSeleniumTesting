0850961b6b0d3c81c7bef37872dee9d15dbcf363
Roberto.Aranda <Robertoaranda133@gmail.com>
2022-11-25 11:49:30 +0100

    Added all test call classes to MainPageTest.java and delete useless classes

--- src/test/java/com/group1/bankassesmentseleniumtesting/MainPageTest.java
+++ src/test/java/com/group1/bankassesmentseleniumtesting/MainPageTest.java
@@ -1,20 +1,12 @@
 package com.group1.bankassesmentseleniumtesting;
 
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.Selenide;
-import com.codeborne.selenide.WebDriverRunner;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.*;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-import static com.codeborne.selenide.Condition.attribute;
-import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
 
 public class MainPageTest {
-    MainPage mainPage = new MainPage();
 
     @BeforeAll
     public static void setUpAll() {
@@ -28,41 +20,43 @@ public class MainPageTest {
     }
 
     @Test
-    public void loginTest(){
-        $("button[data-test='Sign in']").click();
-
-        $("[data-test='username-input']").sendKeys("solera@solera.com");
-        $("[data-test='password-input']").sendKeys("bootcamp4");
-
-        $("button[data-test='Login']").click();
-
-        assertEquals("http://localhost:3000/userAccount", WebDriverRunner.getWebDriver().getCurrentUrl());
-    }
-    /*@Test
-    public void search() {
-        mainPage.searchButton.click();
-
-        $("[data-test='search-input']").sendKeys("Selenium");
-        $("button[data-test='full-search-button']").click();
-
-        $("input[data-test='search-input']").shouldHave(attribute("value", "Selenium"));
+    void loginTest(){
+        LoginTest.loginTest();
     }
 
     @Test
-    public void toolsMenu() {
-        mainPage.toolsMenu.click();
-
-        $("div[data-test='main-submenu']").shouldBe(visible);
+    void registerTest(){
+        RegisterTest.registerTest();
     }
 
     @Test
-    public void navigationToAllTools() {
-        mainPage.seeAllToolsButton.click();
+    void UserSettingTest(){
+        UserSettingTest.userSettingTest();
+    }
 
-        $("#products-page").shouldBe(visible);
+    @Test
+    void BankAccountTest(){
+        BankAccountTest.bankAccountTest();
+    }
 
-        assertEquals("AppCode", Selenide.title());
-    }*/
+    @Test
+    void BankAccountCreateTest(){
+        BankAccountCreateTest.bankAccountCreateTest();
+    }
 
+    @Test
+    void BankAccountDeleteTest(){
+        BankAccountDeleteTest.bankAccountDeleteTest();
+    }
+
+    @Test
+    void NotificationMineTest(){
+        NotificationMineTest.notificationMineTest();
+    }
+
+    @Test
+    void CheckTransactionTest(){
+        CheckTransactionTest.checkTransactionTest();
+    }
 }
 
